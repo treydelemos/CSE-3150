ab117d0cd514d8cf430dcb8308ddd23cebdc777c
Trey Delemos <treydelemos@Treys-MacBook-Pro.local>
2025-09-02 13:40:27 -0400

    First commit

---
+++ week2/ints/int_convert.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int main(){
+    cout << "Enter Score: ";
+    int score;
+    cin >> score;
+    cout << "Score is " << score << endl;
+    return 0;
+}
---
+++ week2/ints/str2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main() {
+    cout << string("Hello") + "world" << endl;
+    return 0;
+    }
+
+
