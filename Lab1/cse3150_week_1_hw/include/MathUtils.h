5bebaeac4c91e8b15058e87c0c763f26f0e28792
Trey Delemos <treydelemos@Treys-MacBook-Pro.local>
2025-09-08 14:53:05 -0400

    fix

---
+++ Lab1/cse3150_week_1_hw/include/MathUtils.h
@@ -0,0 +1,8 @@
+#ifndef MATHUTILS
+#define MATHUTILS
+
+namespace MathUtils{
+    int add(int a, int b);
+    int multiply(int a, int b);
+    }
+#endif
---
+++ Lab1/cse3150_week_1_hw/src/MathUtils.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "AdvancedMath.h"
+#include "MathUtils.h"
+
+
+    namespace MathUtils {
+        int add(int a, int b){
+            return a + b;
+        }
+
+        int multiply(int a, int b){
+            return a * b;
+        }
+    }
+
